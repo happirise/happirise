60f79dc2d344f85dc4c742451b906feaedb08404
Jason <53788601+JeongJaeSoon@users.noreply.github.com>
2024-08-05 02:26:39 +0900

    fix: Modify home_main and mobile navigation (#10)

--- src/app/home/components/Top.tsx
+++ src/app/home/components/Top.tsx
@@ -1,4 +1,9 @@
-import { Pacifico } from 'next/font/google';
+import { Noto_Serif, Pacifico } from 'next/font/google';
+
+const notoSerif = Noto_Serif({
+  subsets: ['latin'],
+  weight: ['900'],
+});
 
 const pacifico = Pacifico({
   subsets: ['latin'],
@@ -11,12 +16,17 @@ export default function Top() {
       className={`h-[calc(100vh-3rem)] lg:mx-12 mr-9 overflow-hidden bg-home_main bg-cover bg-center bg-no-repeat`}
     >
       <div
-        className={`font-black lg:text-7xl text-3xl text-white ${pacifico.className} bg-black bg-opacity-30 w-full h-full flex items-center`}
+        className={`text-white bg-black bg-opacity-30 w-full h-full flex items-center`}
       >
         <span className="m-auto text-center">
-          Corporate recruitment
-          <br className="lg:hidden" />
-          support company
+          <p className={`lg:text-7xl text-3xl ${notoSerif.className}`}>
+            Happirise
+          </p>
+          <p
+            className={`lg:text-4xl text-lg lg:mt-10 mt-4 ${pacifico.className}`}
+          >
+            Corporate recruitment support company
+          </p>
         </span>
       </div>
     </div>
