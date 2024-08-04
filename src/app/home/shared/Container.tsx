37ae83e41ef24eb33aae0bdf4fa2cff4f4529905
JeongJaeSoon <jaesoon-jeong@freee.co.jp>
2024-08-05 02:10:28 +0900

    fix: Modify title margin size

--- src/app/home/shared/Container.tsx
+++ src/app/home/shared/Container.tsx
@@ -1,7 +1,3 @@
-const MARGIN_X_SIZE = 5;
-const MARGIN_LEFT = `ml-${MARGIN_X_SIZE}`;
-const MARGIN_RIGHT = `mr-${MARGIN_X_SIZE}`;
-
 type Props = Readonly<{
   id: string;
   title: string;
@@ -12,13 +8,13 @@ type Props = Readonly<{
 export default function Container({ id, title, children, mt }: Props) {
   return (
     <div id={id} className="lg:mx-12 mr-9 lg:my-24 mt-10 mb-16">
-      <div className={`lg:w-[750px] lg:m-auto ${MARGIN_LEFT}`}>
+      <div className={`lg:w-[750px] lg:mx-auto ml-5`}>
         <h2 className="font-bold text-lg lg:text-xl py-2 w-8 pr-8 lg:pr-9 flex items-center text-wrap border-r-2 border-black">
           <span className="leading-5 lg:leading-6">{title}</span>
         </h2>
       </div>
       <div
-        className={`lg:w-[560px] lg:pr-0 lg:m-auto ml-16 ${mt} lg:p-0 pl-2 ${MARGIN_RIGHT} text-sm leading-9 lg:text-base lg:leading-10`}
+        className={`lg:w-[560px] lg:mx-auto ml-16 mr-5 ${mt} lg:p-0 pl-2 text-sm leading-9 lg:text-base lg:leading-10`}
       >
         {children}
       </div>
